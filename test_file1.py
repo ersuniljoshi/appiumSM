83b05455e520602ccfa7f18c899c542d6307b093
vishal-kor <vishal.kor@infobeans.com>
2017-01-02 18:08:03 +0530

    Two test case files added

---
+++ test_file1.py
@@ -0,0 +1,5 @@
+def test_one():
+    assert 3==3
+
+def test_two():
+    assert 5==5
---
+++ test_file2.py
@@ -0,0 +1,8 @@
+import pytest
+
+@pytest.mark.failed
+def test_3():
+    assert 2==4
+
+def test_4():
+    assert 4==4
